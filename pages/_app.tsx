b551aea7bf3542441520f04cc3a9b11317fd77b5
Wasilewski <e-jbww@gft.com>
2024-01-09 14:59:30 +0100

    _app fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,8 @@
-import Home, {HomeProps, ScrapeConfig} from "./home";
-import {getJobGroupNames, getScrapeConfigs, isAuthorized} from "../requests";
-import getConfig from "next/config";
 import { wrapper } from "../redux/store";
 import { ClientStoreProvider } from "../components/ClientStoreProvider";
 
-export const getServerSideProps = async ({req}): Promise<{props: HomeProps}> => {
-    let _configs: ScrapeConfig[] = [];
-    let _groupNames: string[] = [];
-    let _auth: string | null = null;
-    const { publicRuntimeConfig } = getConfig();
-    await getScrapeConfigs(false, req.headers.cookie).then(resp => {
-        if (resp.status === 200 && resp.data.data && resp.data.data.getPages && resp.data.data.getPages.length)
-            _configs = resp.data.data.getPages;
-    }).catch(e => console.log(e));
-    await getJobGroupNames(false, req.headers.cookie).then(resp => {
-        if (resp.status === 200 && resp.data.data && resp.data.data.getGroupNames && resp.data.data.getGroupNames.names)
-            _groupNames = resp.data.data.getGroupNames.names;
-    }).catch(e => console.log(e));
-    _auth = await isAuthorized(false, req.headers.cookie);
-    return { props: { _auth, _configs, _groupNames, nodeServerHost: publicRuntimeConfig.nodeServerHost || null, springServerHost: publicRuntimeConfig.springServerHost || null } };
+const MyApp = ({ Component, pageProps }) => {
+    return <ClientStoreProvider><Component {...pageProps} /></ClientStoreProvider>
 }
 
-const App = (props) => <ClientStoreProvider><Home {...props}/></ClientStoreProvider>;
-
-export default wrapper.withRedux(App);
+export default wrapper.withRedux(MyApp);
---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import Home, {HomeProps, ScrapeConfig} from "./home";
+import {getJobGroupNames, getScrapeConfigs, isAuthorized} from "../requests";
+import getConfig from "next/config";
+
+export const getServerSideProps = async ({req}): Promise<{props: HomeProps}> => {
+    let _configs: ScrapeConfig[] = [];
+    let _groupNames: string[] = [];
+    let _auth: string | null = null;
+    const { publicRuntimeConfig } = getConfig();
+    await getScrapeConfigs(false, req.headers.cookie).then(resp => {
+        if (resp.status === 200 && resp.data.data && resp.data.data.getPages && resp.data.data.getPages.length)
+            _configs = resp.data.data.getPages;
+    }).catch(e => console.log(e));
+    await getJobGroupNames(false, req.headers.cookie).then(resp => {
+        if (resp.status === 200 && resp.data.data && resp.data.data.getGroupNames && resp.data.data.getGroupNames.names)
+            _groupNames = resp.data.data.getGroupNames.names;
+    }).catch(e => console.log(e));
+    _auth = await isAuthorized(false, req.headers.cookie);
+    return { props: { _auth, _configs, _groupNames, nodeServerHost: publicRuntimeConfig.nodeServerHost || null, springServerHost: publicRuntimeConfig.springServerHost || null } };
+}
+
+const App = (props) => <Home {...props}/>;
+
+export default App;
