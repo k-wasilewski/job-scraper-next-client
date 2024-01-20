1250425d344cd57cf7a228be70ca53d4312e02e7
Wasilewski <e-jbww@gft.com>
2024-01-20 18:28:44 +0100

    forwarding envs in next.config

--- next.config.js
+++ next.config.js
@@ -4,4 +4,8 @@ module.exports = {
     nodeServerHost: process.env.NEXT_PUBLIC_NODE_SERVER_HOST,
     springServerHost: process.env.NEXT_PUBLIC_SPRING_SERVER_HOST,
   },
+  env: {
+    NEXT_PUBLIC_NODE_SERVER_HOST: process.env.NEXT_PUBLIC_NODE_SERVER_HOST,
+    NEXT_PUBLIC_SPRING_SERVER_HOST: process.env.NEXT_PUBLIC_SPRING_SERVER_HOST,
+  }
 }
