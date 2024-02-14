0adb375ef2570b711ae0e7f3494e7ca6908464bf
Wasilewski <e-jbww@gft.com>
2024-02-14 12:02:08 +0100

    added /hooks & RWD for Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,10 +3,14 @@ import { setToLocalStorage } from "../utils/themeService";
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import { isLoading, selectTheme, setTheme, Theme } from "../redux/slices";
+import styles from "../styles/Navbar.module.css";
+import { useWidth } from "../hooks/useWidth";
 
 const Navbar = () => {
     const theme = useSelector(selectTheme);
     const loading: boolean = useSelector(isLoading);
+
+    const screenWidth = useWidth();
 
     const dispatch = useDispatch();
 
@@ -16,6 +20,8 @@ const Navbar = () => {
         dispatch(setTheme(theme));
         setToLocalStorage(theme);
     }
+
+    const dropdownClass = () => screenWidth > 991 ? 'dropdown' : 'dropend';
 
     const isDark = theme === Theme.Dark;
 
@@ -29,6 +35,10 @@ const Navbar = () => {
         <nav className={`navbar navbar-expand-lg navbar-${isDark ? 'dark' : 'light'}`}>
             <a className={`navbar-brand ${isDark ? 'text-white' : ''}`}>job-scraper</a>
 
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item mx-5">
@@ -39,7 +49,7 @@ const Navbar = () => {
                     {loading && loader}
                 </ul>
                 <ul className="navbar-nav justify-content-end ms-auto">
-                    <li className="nav-item dropdown mx-5">
+                    <li className={`nav-item btn-group ${dropdownClass()} mx-5 ${styles.theme_dropdown}`}>
                         <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown">
                             Theme
                         </a>
